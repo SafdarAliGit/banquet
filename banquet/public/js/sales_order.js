db7feeb215606c9f5c5d11ce5a17ce2861a31bd0
safdar211ali <safdar211@hotmail.com>
2025-02-05 20:37:45 +0500

    updated

---
+++ banquet/public/js/sales_order.js
@@ -0,0 +1,32 @@
+frappe.ui.form.on('Sales Order', {
+    refresh(frm) {
+        
+    },
+    custom_hst:function(frm){
+        if(frm.doc.custom_hst){
+            set_service_charges(frm);
+        }
+    },
+    custom_cost:function(frm){
+        if(frm.doc.custom_cost){
+            set_service_charges(frm);
+        }
+    },
+    custom_deposit:function(frm){
+        if(frm.doc.custom_deposit){
+            set_balance(frm);
+        }
+    }
+})
+
+function set_service_charges(frm){ 
+    let custom_hst = cint(frm.doc.custom_hst) || 0;
+    let custom_cost = cint(frm.doc.custom_cost) || 0;
+    frm.set_value("custom_service_charges", custom_hst + custom_cost);
+}
+
+function set_balance(frm){ 
+    let service_charges = cint(frm.doc.custom_service_charges) || 0;
+    let deposit = cint(frm.doc.custom_deposit) || 0;
+    frm.set_value("custom_balance_", service_charges - deposit);
+}
